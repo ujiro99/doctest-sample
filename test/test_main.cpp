df8d3b54ffb57599e1cd021db04afecfe82de6e9
ujiro99 <siro.cola@gmail.com>
2018-01-03 23:08:04 +0900

    [fix] simplify

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,40 +1,10 @@
-#define DOCTEST_CONFIG_IMPLEMENT
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
 #define main _old_main
-
 #include "main.cpp"
-
-#undef main
-#define test_main main
-
-int test_main(int argc, char** argv) {
-	fprintf(stdout, "test start\n");
-    doctest::Context context;
-
-    // !!! THIS IS JUST AN EXAMPLE SHOWING HOW DEFAULTS/OVERRIDES ARE SET !!!
-
-    // defaults
-    context.addFilter("test-case-exclude", "*math*"); // exclude test cases with "math" in their name
-    context.setOption("abort-after", 5);              // stop test execution after 5 failed assertions
-    context.setOption("order-by", "name");            // sort the test cases by their name
-
-    context.applyCommandLine(argc, argv);
-
-    // overrides
-    context.setOption("no-breaks", true);             // don't break in the debugger when assertions fail
-
-    int res = context.run(); // run
-
-    if(context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
-        return res;          // propagate the result of the tests
-    
-    int client_stuff_return_code = 0;
-    // your program - if the testing framework is integrated in your production code
-    
-    return res + client_stuff_return_code; // the result from doctest is propagated here as well
-}
-
+#include "counter.cpp"
+#include "fibonacci.cpp"
 
 TEST_CASE("main") {
     CHECK(_old_main(0, 0) == 0);
